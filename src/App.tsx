4d5236bfbf0e041ebbcfe4d2f8959696f03d8e7c
MelqonHovhannisyan <hovhannisian.melqon@gmail.com>
2023-09-07 15:53:33 +0400

    Added background sound

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { useState } from 'react';
 import { Game } from "@components/Game";
 
 function App() {
+  const [clicked, setClicked] = useState(false);
+
+  const playSound = () => {
+    const audio = new Audio('./audios/background.mp3');
+    audio.play();
+  };
+
+  const handleGameClick = () => {
+    if (!clicked) {
+      playSound();
+      setClicked(true);
+    }
+  };
   return (
-    <div className='min-h-screen flex justify-center'>
+    <div className='min-h-screen flex justify-center' onClick={handleGameClick}>
       <div className='w-full max-w-screen-xl'>
         <Game />
       </div>
